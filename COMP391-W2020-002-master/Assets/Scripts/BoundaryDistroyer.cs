19531e3dbb87163f25be16f655fdb486ea9e10e6
Jerome <nathan_9_9@hotmail.com>
2020-02-21 09:34:01 -0500

    Added Asteroid

---
+++ COMP391-W2020-002-master/Assets/Scripts/BoundaryDistroyer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryDistroyer : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // Destry The other Object
+        Destroy(other.gameObject);
+    }
+}
